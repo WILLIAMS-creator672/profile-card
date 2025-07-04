e0aa7dd44098bbb1989ae07a7e1ec4038f5a3823
WILLIAMS-creator672 <wisdomwills1@outlook.com>
2025-07-04 14:13:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let editButton = document.getElementById('edit-button')
+let bioText = document.getElementById('bio-text')
+let editModal = document.getElementById('edit-modal')
+let editInput = document.getElementById('edit-input')
+let cancelButton = document.getElementById('cancel-button')
+let saveButton = document.getElementById('save-button')
+let overLay = document.getElementById('overlay')
+
+function triggerEditModal() {
+    editModal.style.display = 'block'
+    overLay.style.display = 'block'
+    editInput.value = bioText.innerText
+}
+
+function closeEditModal() {
+    editModal.style.display = 'none'
+    overLay.style.display = 'none'
+}
+
+function saveBio() {
+    closeEditModal()
+    bioText.innerText = editInput.value
+    overLay.style.display = 'none'
+}
+
+editButton.addEventListener('click', () => {
+    triggerEditModal()
+})
+
+cancelButton.addEventListener('click', () => {
+    closeEditModal()
+})
+
+saveButton.addEventListener('click', () => {
+    saveBio()
+})
